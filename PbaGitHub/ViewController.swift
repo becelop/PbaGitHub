caa5f87bd7e1832726ac2486652e6cade50980c6
Gustavo B <gbl@macbook-air-de-gustavo.local>
2019-03-06 14:59:26 -0600

    UPDATE: Generación en código de constraints

--- PbaGitHub/ViewController.swift
+++ PbaGitHub/ViewController.swift
@@ -15,9 +15,33 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+        
+        let viewRoja = UIView()
+        
+        viewRoja.backgroundColor = .red
+        
+        view.addSubview(viewRoja)
+        
+        viewRoja.translatesAutoresizingMaskIntoConstraints = false
+        
+        viewRoja.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        viewRoja.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        viewRoja.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        viewRoja.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
 
+    }
 
 }
 
+extension UIView {
+    func anchor(top: NSLayoutYAxisAnchor, leading: NSLayoutXAxisAnchor, traiding: NSLayoutXAxisAnchor, bottom: NSLayoutYAxisAnchor){
+        
+        translatesAutoresizingMaskIntoConstraints = false
+
+        topAnchor.constraint(equalTo: top).isActive = true
+        leadingAnchor.constraint(equalTo: leading).isActive = true
+        trailingAnchor.constraint(equalTo: trailing).isActive = true
+        bottomAnchor.constraint(equalTo: bottom).isActive = true
+        
+    }
+}
